f1c2cae14806a0a6abd33034a7cf166cd550bf29
Billy <billy_b_97@hotmail.com>
2022-12-16 12:16:16 +1100

    First commit add 1 2 4 and 5 but not 3 6 and 7

---
+++ README.md
@@ -0,0 +1,47 @@
+This web app is being used for a moch tech interview. And must meet the following 5 requirements:
+
+1. Have a pre-populated (hard coded) array of objects as products.
+/* I beleive i achived this goal as the 4 products display on the page*/
+
+2. Your product display should have (not limited to) product image,
+product name, product price
+/* Each product has an img, name, description price and add to cart button*/
+
+3. All products should be displayed when the page loads
+/* the products on screen load along with button functionality however cart is not stored in local */
+
+4. Using an ‘add to cart’ button, the user should be able to add the
+products to a shopping cart list
+/* Items are added to cart when button is clicked */
+
+5. The shopping cart lists all products and displays a the total price
+/* price is totaled after the add to cart button is clicked and every 5 seconds*/
+
+
+========================================
+The following are "Stretch Goals":
+
+6. The user should also be able to delete the products from the cart, thus
+modifying the total price of the cart
+
+
+7. The user should be able to enter a quantity for each product
+
+
+========================================
+One should be able to download the github repository and open the app using shop.html.
+
+This app uses:
+- HTML
+- CSS
+- Vanilla JavaScript
+
+========================================
+
+What I would do if I had more time/continued the project:
+- I would attempt to meet the stretch goals:
+    + Add a remove/Delete button to remove from cart
+    + Add quantity to add multiple of one item without making multiple <divs>. I would also probably change it so instead it adds to the quantity rather than adding a new div.
+- Neaten it up
+- Add a purchase button that empties the cart.
+    + In a larger project I would go to a different page and have a purchase details page/popup.
---
+++ js-modules/storecart.js
@@ -0,0 +1,87 @@
+/*functions that trigger on loading in */
+window.addEventListener('load', loadingInFunc);
+
+function loadingInFunc() {
+    let addToCartBtns = document.getElementsByClassName('bBtn');
+    for (let i = 0; i < addToCartBtns.length; i++) {
+        let button = addToCartBtns[i];
+        button.addEventListener('click', addToCart);
+        button.addEventListener('click', CalcPrice);
+        button.addEventListener('click', storeDat);
+    };
+};
+
+/*Add to cart function(s) below */
+
+function addToCart(event) {
+    let button = event.target
+    let boughtItem = button.parentElement.parentElement
+    let iName = boughtItem.getElementsByClassName("itemName")[0].innerText
+    let iPrice = boughtItem.getElementsByClassName("Price")[0].innerText
+    let iImg = boughtItem.getElementsByClassName("prodImg")[0].src
+
+    mvTCart(iName, iPrice, iImg)
+};
+
+function mvTCart(iName, iPrice, iImg) {
+    
+    let space = document.createElement("div")
+    space.classList.add('cartRow')
+
+    let sCart = document.getElementsByClassName("shoppingCart")[0]
+
+    let selItemCont = `
+    <div class="cartItem cartCol">
+        <img class="cartItemImg" src="${iImg}" width="50" height="50">
+        <span class="cartItemTitle">${iName}</span>
+    </div>
+    <span class="cartPrice cartCol">${iPrice}</span>`;
+
+    space.innerHTML = selItemCont;
+
+    sCart.append(space);
+}
+
+
+/* Calculate Price total below*/
+
+setInterval(CalcPrice, 5000);
+
+function CalcPrice () {
+    let totalP = 0;
+
+    let cartS = document.getElementsByClassName("shoppingCart")[0];
+    
+    let itemRows = cartS.getElementsByClassName("cartRow");
+
+    for (let j = 0; j < itemRows.length; j++) {
+        let CRow = itemRows[j];
+        let priceCl = CRow.getElementsByClassName('cartPrice')[0];
+        /* got the replace from the coding of the del btn in
+        sprint 3 */
+        let price = parseFloat(priceCl.innerText.replace('$', ''));
+
+        totalP = totalP + price;
+    }
+
+    totalP = Math.round(totalP * 100) / 100;
+
+    document.getElementsByClassName("cartTotal")[0].innerText = `Cart Total = `+`$`+`${totalP}`;
+};
+
+
+/*Local storage below */
+
+// function storeDat() {
+
+//     let bagItems = document.getElementsByClassName('cartRow');
+    
+    
+//     let cartArray = Array.from(bagItems);
+//     console.log(cartArray);
+//     localStorage.setItem("cart", JSON.stringify(cartArray));
+// };
+
+// function showDat() {
+//     localStorage.getItem("cart")
+// }
